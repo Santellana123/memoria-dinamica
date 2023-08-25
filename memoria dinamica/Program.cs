93e05a89a587b4f1223f4301729edbeaa11d479b
Santellana179 <imjuan179@gmail.com>
2023-08-25 02:39:15 -0500

    Agregar archivos de proyecto.

---
+++ memoria dinamica/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Ingrese el tamaño de la lista: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+List<int> lista = new List<int>();
+
+Console.WriteLine("Ingrese los elementos de la lista:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Elemento {i + 1}: ");
+    int elemento = Convert.ToInt32(Console.ReadLine());
+    lista.Add(elemento);
+}
+
+Console.WriteLine("Los elementos ingresados son:");
+foreach (int elemento in lista)
+{
+    Console.Write(elemento + " ");
+}
+Console.WriteLine();
